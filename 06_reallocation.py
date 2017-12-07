c539969ceff5bc7a5f2b1d1df6e19971047fa174
XIIIvs <xiii_1994@wp.pl>
2017-12-07 11:44:21 +0100

    Day 6: Memory Reallocation

---
+++ 06_reallocation.py
@@ -0,0 +1,23 @@
+banks = [11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11]
+
+steps = int()
+answers = list()
+ans = tuple(banks)
+print(ans, "steps =", steps)
+
+indexes = len(banks)
+
+while ans not in answers:
+    answers.append(ans)
+    steps += 1
+    blocks = max(banks)
+    index = banks.index(blocks)
+    banks[index] = 0
+
+    for blck in range(blocks):
+        index += 1
+        index %= indexes
+        banks[index] += 1
+
+    ans = tuple(banks)
+    print(ans, "steps =", steps)
